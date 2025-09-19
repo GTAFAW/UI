4307ebd748b0ae14fd562096230bc6349efe9737
xxxxxxg <GTAFAW19516968793@qq.com>
2025-09-19 16:10:35 +0800

    Add files via upload

--- UI.-.XGOHUB.lua
+++ UI.-.XGOHUB.lua
@@ -3245,19 +3245,19 @@ end;
 ------------------------------//    UI.标题设置    //-------------------------------------------------------------------------------------
 
 function Library:Windowxgo(setup)
-    setup = setup or {}
-    setup.Title = setup.Title or "Window"
-    setup.Keybind = setup.Keybind or Enum.KeyCode.LeftControl
-    setup.Size = setup.Size or Library.SizeLibrary.Default
-    setup.KeySystem = setup.KeySystem or false
-    setup.Logo = setup.Logo or "rbxassetid://7733920644"
-    setup.ToggleMethod = setup.ToggleMethod or "Application"
+    setup = setup or {};
+    setup.Title = setup.Title or "Window";
+    setup.Keybind = setup.Keybind or Enum.KeyCode.LeftControl;
+    setup.Size = setup.Size or Library.SizeLibrary.Default;
+    setup.KeySystem = setup.KeySystem or false;
+    setup.Logo = setup.Logo or "rbxassetid://7733920644";
+    setup.ToggleMethod = setup.ToggleMethod or "Application";
 
     if setup.KeySystem then
-        setup.KeySystemInfo = setup.KeySystemInfo or {}
-        setup.KeySystemInfo.Title = setup.KeySystemInfo.Title or "Key System"
-        setup.KeySystemInfo.OnGetKey = setup.KeySystemInfo.OnGetKey or function() end
-        setup.KeySystemInfo.OnLogin = setup.KeySystemInfo.OnLogin or function() wait(0.1) return true end
+        setup.KeySystemInfo = setup.KeySystemInfo or {};
+        setup.KeySystemInfo.Title = setup.KeySystemInfo.Title or "Key System";
+        setup.KeySystemInfo.OnGetKey = setup.KeySystemInfo.OnGetKey or function() end;
+        setup.KeySystemInfo.OnLogin = setup.KeySystemInfo.OnLogin or function() wait(0.1) return true end;
     end
 
     local ScreenGui = Instance.new("ScreenGui")
@@ -3268,20 +3268,95 @@ function Library:Windowxgo(setup)
     local Ico = Instance.new("ImageLabel")
 
     local images = {
-        "rbxassetid://113180426865309","rbxassetid://127110756366496","rbxassetid://131471211520335","rbxassetid://102428625138886","rbxassetid://137332235358973","rbxassetid://109520199976167","rbxassetid://122958225353990","rbxassetid://140434608197988","rbxassetid://124568548523146","rbxassetid://76544783777123","rbxassetid://117347134697321","rbxassetid://98554545052070","rbxassetid://131266490485584","rbxassetid://131496206276220","rbxassetid://109752539854084","rbxassetid://70869793345513","rbxassetid://81618536870963","rbxassetid://96996068161496","rbxassetid://92875796313338","rbxassetid://89067121877834","rbxassetid://113186601959200","rbxassetid://73164000772284","rbxassetid://101305557601423","rbxassetid://104067029112641","rbxassetid://110630445580007","rbxassetid://108644687915512","rbxassetid://86540442133927","rbxassetid://107018855884545","rbxassetid://112966984941036","rbxassetid://80598426066070","rbxassetid://108391089326665","rbxassetid://138013328013091","rbxassetid://110990525726887","rbxassetid://72760885562855","rbxassetid://82117275977223","rbxassetid://80695281901172","rbxassetid://123238115911519","rbxassetid://135035617747628","rbxassetid://74297023080665","rbxassetid://84550607098866","rbxassetid://132992677171078","rbxassetid://126722695077251","rbxassetid://82915814593812","rbxassetid://105485276493469","rbxassetid://107983351429754","rbxassetid://121439238665385","rbxassetid://135083937747387","rbxassetid://130643405858214","rbxassetid://102409437463046","rbxassetid://87780505852956","rbxassetid://137945894459284","rbxassetid://101196101249400","rbxassetid://73934456979168","rbxassetid://124323332173934","rbxassetid://71240399712190","rbxassetid://127390304118462","rbxassetid://131409006813490","rbxassetid://80253796704859","rbxassetid://129206776380514","rbxassetid://121697617411442","rbxassetid://129410104830757","rbxassetid://71101554362190","rbxassetid://117937637678090","rbxassetid://89768207500333","rbxassetid://136363102949077","rbxassetid://74648780628027","rbxassetid://103232778626018","rbxassetid://76127155963189","rbxassetid://118305240093538","rbxassetid://112630176374798","rbxassetid://74804451529535","rbxassetid://115691043156297","rbxassetid://100980082510772","rbxassetid://135027711714247","rbxassetid://113389633674712","rbxassetid://94012779929465"
+        "rbxassetid://113180426865309",
+        "rbxassetid://127110756366496",
+        "rbxassetid://131471211520335",
+        "rbxassetid://102428625138886",
+        "rbxassetid://137332235358973",
+        "rbxassetid://109520199976167",
+        "rbxassetid://122958225353990",
+        "rbxassetid://140434608197988",
+        "rbxassetid://124568548523146",
+        "rbxassetid://76544783777123",
+        "rbxassetid://117347134697321",
+        "rbxassetid://98554545052070",
+        "rbxassetid://131266490485584",
+        "rbxassetid://131496206276220",
+        "rbxassetid://109752539854084",
+        "rbxassetid://70869793345513",
+        "rbxassetid://81618536870963",
+        "rbxassetid://96996068161496",
+        "rbxassetid://92875796313338",
+        "rbxassetid://89067121877834",
+        "rbxassetid://113186601959200",
+        "rbxassetid://73164000772284",
+        "rbxassetid://101305557601423",
+        "rbxassetid://104067029112641",
+        "rbxassetid://110630445580007",
+        "rbxassetid://108644687915512",
+        "rbxassetid://86540442133927",
+        "rbxassetid://107018855884545",
+        "rbxassetid://112966984941036",
+        "rbxassetid://80598426066070",
+        "rbxassetid://108391089326665",
+        "rbxassetid://138013328013091",
+        "rbxassetid://110990525726887",
+        "rbxassetid://72760885562855",
+        "rbxassetid://82117275977223",
+        "rbxassetid://80695281901172",
+        "rbxassetid://123238115911519",
+        "rbxassetid://135035617747628",
+        "rbxassetid://74297023080665",
+        "rbxassetid://84550607098866",
+        "rbxassetid://132992677171078",
+        "rbxassetid://126722695077251",
+        "rbxassetid://82915814593812",
+        "rbxassetid://105485276493469",
+        "rbxassetid://107983351429754",
+        "rbxassetid://121439238665385",
+        "rbxassetid://135083937747387",
+        "rbxassetid://130643405858214",
+        "rbxassetid://102409437463046",
+        "rbxassetid://87780505852956",
+        "rbxassetid://137945894459284",
+        "rbxassetid://101196101249400",
+        "rbxassetid://73934456979168",
+        "rbxassetid://124323332173934",
+        "rbxassetid://71240399712190",
+        "rbxassetid://127390304118462",
+        "rbxassetid://131409006813490",
+        "rbxassetid://80253796704859",
+        "rbxassetid://129206776380514",
+        "rbxassetid://121697617411442",
+        "rbxassetid://129410104830757",
+        "rbxassetid://71101554362190",
+        "rbxassetid://117937637678090",
+        "rbxassetid://89768207500333",
+        "rbxassetid://136363102949077",
+        "rbxassetid://74648780628027",
+        "rbxassetid://103232778626018",
+        "rbxassetid://76127155963189",
+        "rbxassetid://118305240093538",
+        "rbxassetid://112630176374798",
+        "rbxassetid://74804451529535",
+        "rbxassetid://115691043156297",
+        "rbxassetid://100980082510772",
+        "rbxassetid://135027711714247",
+        "rbxassetid://113389633674712",
+        "rbxassetid://94012779929465"
     }
 
     local currentIndex = 1
     local isForward = true
     local slideDuration = 1.5
     local interval = 13.5
-    local paused = false
-    local switching = false
 
     local readyToLoad = Instance.new("BindableEvent")
     local nextToPreload = 2
 
-    task.spawn(function() game:GetService("ContentProvider"):PreloadAsync(images) end)
+    task.spawn(function()
+        game:GetService("ContentProvider"):PreloadAsync(images)
+    end)
 
     local preloader = Instance.new("ImageLabel")
     preloader.Visible = false
@@ -3291,8 +3366,8 @@ function Library:Windowxgo(setup)
         local first = images[currentIndex]
         BackgroundImage1.Parent = MainFrame
         BackgroundImage1.BackgroundTransparency = 1
-        BackgroundImage1.Size = UDim2.new(1,0,1,0)
-        BackgroundImage1.Position = UDim2.new(0,0,0,0)
+        BackgroundImage1.Size = UDim2.new(1, 0, 1, 0)
+        BackgroundImage1.Position = UDim2.new(0, 0, 0, 0)
         BackgroundImage1.Image = first
         BackgroundImage1.ScaleType = Enum.ScaleType.Stretch
         BackgroundImage1.ImageTransparency = 0
@@ -3300,26 +3375,29 @@ function Library:Windowxgo(setup)
 
         BackgroundImage2.Parent = MainFrame
         BackgroundImage2.BackgroundTransparency = 1
-        BackgroundImage2.Size = UDim2.new(1,0,1,0)
-        BackgroundImage2.Position = UDim2.new(1,0,0,0)
+        BackgroundImage2.Size = UDim2.new(1, 0, 1, 0)
+        BackgroundImage2.Position = UDim2.new(1, 0, 0, 0)
         BackgroundImage2.Image = first
         BackgroundImage2.ImageTransparency = 0
         BackgroundImage2.ScaleType = Enum.ScaleType.Stretch
         BackgroundImage2.ZIndex = 2
     end
 
-    local function getNextIndex(idx,fwd)
-        if fwd then return idx==#images and #images-1 or idx+1
-        else return idx==1 and 2 or idx-1 end
+    local function getNextIndex(idx, fwd)
+        if fwd then
+            return idx == #images and #images - 1 or idx + 1
+        else
+            return idx == 1 and 2 or idx - 1
+        end
     end
-
+    
     task.spawn(function()
         while true do
             readyToLoad.Event:Wait()
-            local target = getNextIndex(currentIndex,isForward)
-            if (isForward and currentIndex==#images) or (not isForward and currentIndex==1) then
+            local target = getNextIndex(currentIndex, isForward)
+            if (isForward and currentIndex == #images) or (not isForward and currentIndex == 1) then
                 isForward = not isForward
-                target = getNextIndex(currentIndex,isForward)
+                target = getNextIndex(currentIndex, isForward)
             end
             preloader.Image = images[target]
             readyToLoad:Fire()
@@ -3328,33 +3406,30 @@ function Library:Windowxgo(setup)
     end)
 
     local function slideSwitch()
-        if switching then return end
-        switching = true
-        local nextIndex = getNextIndex(currentIndex,isForward)
+        local nextIndex = getNextIndex(currentIndex, isForward)
         BackgroundImage2.Image = images[nextIndex]
 
-        local startPos = UDim2.new(1,0,0,0)
-        local endPos   = UDim2.new(0,0,0,0)
-        local oldEndPos= UDim2.new(-1,0,0,0)
+        local startPos = UDim2.new(1, 0, 0, 0)
+        local endPos   = UDim2.new(0, 0, 0, 0)
+        local oldEndPos= UDim2.new(-1, 0, 0, 0)
         if not isForward then
-            startPos = UDim2.new(-1,0,0,0)
-            oldEndPos= UDim2.new(1,0,0,0)
+            startPos = UDim2.new(-1, 0, 0, 0)
+            oldEndPos= UDim2.new(1, 0, 0, 0)
         end
 
         BackgroundImage2.Position = startPos
 
-        Library:Tween(BackgroundImage2,Library.TweenLibrary.SmallEffect,{Position=endPos},slideDuration)
-        Library:Tween(BackgroundImage1,Library.TweenLibrary.SmallEffect,{Position=oldEndPos},slideDuration)
+        Library:Tween(BackgroundImage2, Library.TweenLibrary.SmallEffect, {Position = endPos}, slideDuration)
+        Library:Tween(BackgroundImage1, Library.TweenLibrary.SmallEffect, {Position = oldEndPos}, slideDuration)
 
         task.wait(slideDuration)
 
         currentIndex = nextIndex
         BackgroundImage1.Image = BackgroundImage2.Image
-        BackgroundImage1.Position = UDim2.new(0,0,0,0)
-        BackgroundImage2.Position = UDim2.new(1,0,0,0)
+        BackgroundImage1.Position = UDim2.new(0, 0, 0, 0)
+        BackgroundImage2.Position = UDim2.new(1, 0, 0, 0)
 
         readyToLoad:Fire()
-        switching = false
     end
 
     ScreenGui.Parent = game.CoreGui
@@ -3365,82 +3440,68 @@ function Library:Windowxgo(setup)
     MainFrame.Name = "MainFrame"
     MainFrame.Parent = ScreenGui
     MainFrame.Active = true
-    MainFrame.AnchorPoint = Vector2.new(0.5,0.5)
-    MainFrame.BackgroundColor3 = Color3.fromRGB(255,255,255)
-    MainFrame.BackgroundTransparency = 0.25
+    MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+    MainFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    MainFrame.BackgroundTransparency = 0.250
     MainFrame.BorderSizePixel = 0
     MainFrame.ClipsDescendants = true
-    MainFrame.Position = UDim2.new(0.5,0,0.5,0)
-    MainFrame.Size = UDim2.fromScale(0,0)
+    MainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+    MainFrame.Size = UDim2.fromScale(0, 0)
 
     initBackgrounds()
 
     task.spawn(function()
         while true do
-            if not paused then
-                task.wait(interval)
-                if not paused then slideSwitch() end
-            else
-                task.wait()
-            end
+            task.wait(interval)
+            slideSwitch()
         end
     end)
 
     readyToLoad:Fire()
 
-    local BlurEle = Library.UIBlur.new(MainFrame,true)
+    local BlurEle = Library.UIBlur.new(MainFrame, true)
 
     DropShadow.Name = "DropShadow"
     DropShadow.Parent = MainFrame
-    DropShadow.BackgroundTransparency = 1
-    DropShadow.Position = UDim2.new(0,-5,0,-5)
-    DropShadow.Rotation = 0.01
-    DropShadow.Size = UDim2.new(1,10,1,10)
+    DropShadow.BackgroundTransparency = 1.000
+    DropShadow.Position = UDim2.new(0, -5, 0, -5)
+    DropShadow.Rotation = 0.010
+    DropShadow.Size = UDim2.new(1, 10, 1, 10)
     DropShadow.ZIndex = -5
     DropShadow.Image = "rbxassetid://297694300"
-    DropShadow.ImageColor3 = Color3.fromRGB(0,0,0)
-    DropShadow.ImageTransparency = 0.5
+    DropShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+    DropShadow.ImageTransparency = 0.500
     DropShadow.ScaleType = Enum.ScaleType.Slice
-    DropShadow.SliceCenter = Rect.new(95,103,894,902)
-    DropShadow.SliceScale = 0.05
+    DropShadow.SliceCenter = Rect.new(95, 103, 894, 902)
+    DropShadow.SliceScale = 0.050
 
     Ico.Name = "Ico"
     Ico.Parent = MainFrame
-    Ico.AnchorPoint = Vector2.new(0.5,0.5)
-    Ico.BackgroundTransparency = 1
+    Ico.AnchorPoint = Vector2.new(0.5, 0.5)
+    Ico.BackgroundTransparency = 1.000
     Ico.BorderSizePixel = 0
-    Ico.Position = UDim2.new(0.5,0,0.5,0)
-    Ico.Size = UDim2.new(0.6,0,0.6,0)
+    Ico.Position = UDim2.new(0.5, 0, 0.5, 0)
+    Ico.Size = UDim2.new(0.600000024, 0, 0.600000024, 0)
     Ico.SizeConstraint = Enum.SizeConstraint.RelativeYY
     Ico.Image = setup.Logo
-    Ico.ImageTransparency = 1
+    Ico.ImageTransparency = 1.000
 
-    Library:Tween(MainFrame,Library.TweenLibrary.SmallEffect,{Size=Library.SizeLibrary.Loading})
-    Library:Tween(Ico,Library.TweenLibrary.SmallEffect,{ImageTransparency=0.25})
+    Library:Tween(MainFrame, Library.TweenLibrary.SmallEffect, {Size = Library.SizeLibrary.Loading})
+    Library:Tween(Ico, Library.TweenLibrary.SmallEffect, {ImageTransparency = 0.25})
 
     if setup.KeySystem then
         setup.KeySystemInfo.Enabled = true
         setup.KeySystemInfo.Finished = Instance.new('BindableEvent')
-        task.wait(1)
-        task.delay(0.1,function()
-            Library:Tween(Ico,Library.TweenLibrary.SmallEffect,{ImageTransparency=1})
-        end)
-        Library:Tween(MainFrame,Library.TweenLibrary.WindowChanged,{Size=Library.SizeLibrary.Auth})
-        task.wait(1);
-    end
 
-    -- 接口    __ C你m
-    Library.Windowxgo = Library.Windowxgo or {}
-    function Library.Windowxgo.PauseToggle() paused = not paused end
-    function Library.Windowxgo.PrevImage()
-        if switching then return end
-        paused = true ; isForward = false ; slideSwitch() ; paused = false
-    end
-    function Library.Windowxgo.NextImage()
-        if switching then return end
-        paused = true ; isForward = true  ; slideSwitch() ; paused = false
-    end
-end
+        task.wait(1)
+
+        task.delay(0.1, function()
+            Library:Tween(Ico, Library.TweenLibrary.SmallEffect, {ImageTransparency = 1})
+        end)
+
+        Library:Tween(MainFrame, Library.TweenLibrary.WindowChanged, {Size = Library.SizeLibrary.Auth})
+
+        task.wait(1);
 ------ // 卡密系统设置    ----------------------------------------------------------------------------------------
 
 		local AuthFunction = Instance.new("Frame")
